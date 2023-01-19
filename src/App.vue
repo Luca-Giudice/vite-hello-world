471b076a95c905f34f61479052bcad9bc4bc5ea2
Luca-Giudice <2giudice4@gmail.com>
2023-01-19 17:10:21 +0100

    import-app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default{
+  data(){
+    name: 'Yeah'
+  },
+  components:{
+    AppTitle: AppTitle
+  }
+
+}
+</script>
+
+<template>
+<AppTitle />
+<p>{{ name }}</p>
+</template>
+
+<style>
+/* stile css */
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data(){
+        return{
+
+            title:'La mia prima App con Vite'
+        }
+    }
+}
+</script>
+
+<template>
+<header>
+    <h1>{{ title }}</h1>
+</header>
+</template>
+<style>
+</style>
